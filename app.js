31c65f18ef0454b91e361d86dfb3d70b38ff6839
hetias <hetiasdou@gmail.com>
2023-06-26 12:34:48 -0300

    added css

--- app.js
+++ app.js
@@ -5,6 +5,46 @@
 //get form input
 //get display elements
 
+//pokemon types background colors
+var typeColors = [
+    {"name":"normal",
+     "color": "#A8A878"},
+    {"name":"fire",
+     "color":"#F08030"},
+    {"name":"water",
+     "color":"#6890F0"},
+    {"name":"electric",
+     "color":"#F8D030"},
+    {"name":"grass",
+     "color":"#78C850"},
+    {"name":"ice",
+     "color": "#98D8D8"},
+    {"name":"fighting",
+     "color":"#C03028"},
+    {"name":"poison",
+     "color":"#A040A0"},
+    {"name":"ground",
+     "color":"#E0C068"},
+    {"name":"flying",
+     "color":"#A890F0"},
+    {"name":"psychic",
+     "color":"#F85888"},
+    {"name":"bug",
+     "color":"#A8B820"},
+    {"name":"rock",
+     "color":"#B8A038"},
+    {"name":"ghost",
+     "color":"#705898"},
+    {"name":"dragon",
+     "color":"#7038F8"},
+    {"name":"dark",
+     "color":"#705848"},
+    {"name":"steel",
+     "color":"#B8B8D0"},
+    {"name":"fairy",
+     "color":"#EE99AC"}
+];
+
 //form elements
 const searchButton = document.getElementById("searchPokemon");
 const searchInput = document.getElementById("pokemonInput");
@@ -55,17 +95,25 @@ async function OnSubmit(){
 //replace display elements values with correct pokemon values
 
 function updateInformation(){
-    pokemonInformation.style.display = "block";
+    pokemonInformation.style.display = "flex";
 
     pokemonSprite.src = pokemonInfo.sprites.front_default;
+    typeColors.forEach(color => {
+	if(pokemonInfo.types[0].type.name == color.name){
+	    console.log("coincide");
+	    pokemonSprite.style.backgroundColor = `${color.color}`;
+	}
 
+    });
+    
     pokemonName.innerHTML = `${pokemonInfo.name}`;
     
     pokemonId.innerHTML = `Id: ${pokemonInfo.id}`;
 
     pokemonWeight.innerHTML = `Weight: ${(pokemonInfo.weight * 0.1).toFixed(1)}Kg`;
-
+    pokemonWeight.setAttribute("class", "element");
     pokemonHeight.innerHTML = `Height: ${(pokemonInfo.height * 0.1).toFixed(2)}cm`;
+    pokemonHeight.setAttribute("class", "element");
     
     let _pokemonTypes = " ";
     pokemonInfo.types.forEach( element =>{
@@ -77,7 +125,7 @@ function updateInformation(){
     pokemonInfo.abilities.forEach( abilities =>{
 	_pokeAbilities += abilities.ability.name + " ";
     });
-    pokemonAbility.innerHTML = `Abilities: ${_pokeAbilities}`;
+    pokemonAbility.innerHTML = `${_pokeAbilities}`;
 
     pokemonStatHp.innerHTML = `${pokemonInfo.stats[0].stat.name}: ${pokemonInfo.stats[0].base_stat}`;
     pokemonStatAttack.innerHTML = `${pokemonInfo.stats[1].stat.name}: ${pokemonInfo.stats[1].base_stat}`;
