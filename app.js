0be6bb29eaa26fcaacc1af072eb608eb51788c43
hetias <hetiasdou@gmail.com>
2023-06-27 06:25:23 -0300

    added types colors and fixed uppercase searchs bug

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ var pokemonInformation = document.getElementById("pokemonInformation");
 var pokemonName = document.getElementById("pokemonName");
 var pokemonWeight = document.getElementById("pokemonWeight");
 var pokemonHeight = document.getElementById("pokemonHeight");
-var pokemonType = document.getElementById("pokemonType");
+var pokemonTypes = document.getElementById("pokemonTypes");
 var pokemonId = document.getElementById("pokemonId");
 var pokemonAbility = document.getElementById("pokemonAbility");
 var pokemonStats = document.getElementById("pokemonStats");
@@ -72,11 +72,11 @@ var errorMessage = document.getElementById("errorMessage");
 async function OnSubmit(){
     //if search is different from nothing
     let subbmitedPokemon = searchInput.value;
-    if(subbmitedPokemon != ""){
+    if((subbmitedPokemon != "")){
 	
 	//get pokemon info and save it on another variable
 	try{
-	    data = await fetch(`https://pokeapi.co/api/v2/pokemon/${subbmitedPokemon}`)		
+	    data = await fetch(`https://pokeapi.co/api/v2/pokemon/${subbmitedPokemon.toLowerCase()}`)		
 	    pokemonInfo = await data.json();
 
 	    console.log(pokemonInfo);
@@ -98,17 +98,10 @@ function updateInformation(){
     pokemonInformation.style.display = "flex";
 
     pokemonSprite.src = pokemonInfo.sprites.front_default;
-    typeColors.forEach(color => {
-	if(pokemonInfo.types[0].type.name == color.name){
-	    console.log("coincide");
-	    pokemonSprite.style.backgroundColor = `${color.color}`;
-	}
-
-    });
     
     pokemonName.innerHTML = `${pokemonInfo.name}`;
     
-    pokemonId.innerHTML = `Id: ${pokemonInfo.id}`;
+    pokemonId.innerHTML = `${pokemonInfo.id}`;
 
     pokemonWeight.innerHTML = `Weight: ${(pokemonInfo.weight * 0.1).toFixed(1)}Kg`;
     pokemonWeight.setAttribute("class", "element");
@@ -116,10 +109,25 @@ function updateInformation(){
     pokemonHeight.setAttribute("class", "element");
     
     let _pokemonTypes = " ";
+    while(pokemonTypes.childNodes.length > 2){
+	pokemonTypes.removeChild(pokemonTypes.lastChild);
+    }
+    
     pokemonInfo.types.forEach( element =>{
-	_pokemonTypes += element.type.name + " ";
+	const _typeText = document.createTextNode(element.type.name);
+	const _node = document.createElement("p");
+
+	typeColors.forEach(typeColor =>{
+	    if(typeColor.name == element.type.name){
+		_node.classList.add("element");
+		_node.style.backgroundColor = `${typeColor.color}`;
+	    }
+	});
+	
+	pokemonTypes.appendChild(_node);
+	_node.appendChild(_typeText);
     });
-    pokemonType.innerHTML = `Type: ${_pokemonTypes}`;
+
 
     let _pokeAbilities = "";
     pokemonInfo.abilities.forEach( abilities =>{
